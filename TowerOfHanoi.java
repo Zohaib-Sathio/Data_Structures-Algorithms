440a72ad69e4359d23e7a51753c377d36ef599ac
Zohaib Hussain <zohaibhussainsathio@gmail.com>
2022-05-09 12:33:22 +0500

    Update TowerOfHanoi.java

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -3,20 +3,20 @@ package data.structures;
 
 class TowerOfHanoi {
     
-     static void Tower(int n, String src, String helper, String dest) {
+     static void Tower(int n, char src, char dest, char helper) {
          if (n == 1) {
              System.out.println("Transfer disk " + n + " From " + src + "  to " + dest);
              return;
          }
-         Tower(n - 1, src, dest, helper);
+         Tower(n - 1, src, helper, desc);
          
          System.out.println("Transfer disk " + n + " From " + src + " to " + dest);
          
-         Tower(n - 1, helper, src, dest);
+         Tower(n - 1, helper, dest, src);
      }
 
      public static void main(String[] args) {
          int n = 3;
-         Tower(n, "S","H","D");
+         Tower(n, 'S','H','D');
      }
  }
